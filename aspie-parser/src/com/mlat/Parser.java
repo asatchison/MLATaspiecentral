1ff9252d78b311bf0fc6b1eb2b8d969b1f3096b9
Yoko Yamaguchi <yokoy@Yokos-MacBook-Air-2.local>
2016-03-13 21:03:23 -0700

    Added Matlabtextformatter

--- aspie-parser/src/com/mlat/Parser.java
+++ aspie-parser/src/com/mlat/Parser.java
@@ -16,7 +16,7 @@ public class Parser {
 
     final static String HTMLSTRING = "AspiesCentral.com Log in or Sign up AspiesCentral.com Home Forums > Announcements & Feedback > Site Questions, Suggestions & Feedback > Welcome to Aspies Central, a friendly forum to discuss Aspergers Syndrome, Autism, High Functioning Autism and related conditions. Your voice is missing! You will need to register to get access to the following site features: Reply to discussions and create your own threads. Our modern chat room. No add-ons or extensions required, just login and start chatting! Private Member only forums for more serious discussions that you may wish to not have guests or search engines access to. Your very own blog. Write about anything you like on your own individual blog. We hope to see you as a part of our community soon! Please also check us out @ https:\\/\\/www.twitter.com\\/aspiescentral";
 
-    private static List<String> getStopwordsFromFile() {
+    public static List<String> getStopwordsFromFile() {
         String line = "";
         List<String> stopwords = new ArrayList<String>();
         try {
@@ -30,59 +30,59 @@ public class Parser {
         return stopwords;
     }
 
-    public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
-        List<String> stopwords = getStopwordsFromFile();
-        BufferedWriter writer = null;
-        try {
-            int size = 0;
-            JSONObject obj = (JSONObject) parser.parse(new FileReader("data/sample.txt"));
-            JSONObject response = (JSONObject) obj.get("response");
-            JSONArray docs = (JSONArray) response.get("docs");
-            while (docs.size() > size && docs.get(size) != null) {
-                JSONObject doc = (JSONObject) docs.get(size);
-                String title = doc.get("title").toString().replace("[", "").replace("]","").replace("\"", "");
-                String url =  (doc.get("url").toString()).replace("\\/", "/").replace("[", "").replace("]","").replace("\"", "");
-                if (!url.endsWith("index.rss")) {
-                    writer = new BufferedWriter(new FileWriter("output/thread" + size + ".txt"));
-                    // Replacing Html tags for every thread
-                    String content = doc.get("content").toString().replace(HTMLSTRING, "");
-
-                    System.out.println("Title: " + title);
-                    System.out.println("URL: " + url);
-
-                    // Replacing all non-alpha characters with empty strings
-                    String filteredContent = content.replaceAll("[^A-Za-z ]", "");
-                    // Separating each string by space and store them into array
-                    String textStr[] = filteredContent.split("\\s+");
-
-                    writer.write("Title: " + title + "\n");
-                    writer.write("URL: " + url + "\n");
-
-                    for (int i=0; i<textStr.length; ++i) {
-                        // Don't include string that has length 1
-                        // Don't include string that are stop words
-                        if (textStr[i].length() > 1 && !stopwords.contains(textStr[i].toLowerCase())) {
-                            writer.write(textStr[i] + "\n");
-                        }
-                    }
-
-                    System.out.println("Successfully wrote to file >" + "thread" + size + ".txt");
-                    System.out.println("----------------------------------------");
-                    writer.close();
-                } else {
-                    // avoid duplicate threads
-                    System.out.println("Skipping " + title + "....");
-                    System.out.println("----------------------------------------");
-
-                }
-                size++;
-            }
-        } catch (ParseException pe) {
-            System.out.println("position: " + pe.getPosition());
-            System.out.println(pe);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        JSONParser parser = new JSONParser();
+//        List<String> stopwords = getStopwordsFromFile();
+//        BufferedWriter writer = null;
+//        try {
+//            int size = 0;
+//            JSONObject obj = (JSONObject) parser.parse(new FileReader("data/sample.txt"));
+//            JSONObject response = (JSONObject) obj.get("response");
+//            JSONArray docs = (JSONArray) response.get("docs");
+//            while (docs.size() > size && docs.get(size) != null) {
+//                JSONObject doc = (JSONObject) docs.get(size);
+//                String title = doc.get("title").toString().replace("[", "").replace("]","").replace("\"", "");
+//                String url =  (doc.get("url").toString()).replace("\\/", "/").replace("[", "").replace("]","").replace("\"", "");
+//                if (!url.endsWith("index.rss")) {
+//                    writer = new BufferedWriter(new FileWriter("output/thread" + size + ".txt"));
+//                    // Replacing Html tags for every thread
+//                    String content = doc.get("content").toString().replace(HTMLSTRING, "");
+//
+//                    System.out.println("Title: " + title);
+//                    System.out.println("URL: " + url);
+//
+//                    // Replacing all non-alpha characters with empty strings
+//                    String filteredContent = content.replaceAll("[^A-Za-z ]", "");
+//                    // Separating each string by space and store them into array
+//                    String textStr[] = filteredContent.split("\\s+");
+//
+//                    writer.write("Title: " + title + "\n");
+//                    writer.write("URL: " + url + "\n");
+//
+//                    for (int i=0; i<textStr.length; ++i) {
+//                        // Don't include string that has length 1
+//                        // Don't include string that are stop words
+//                        if (textStr[i].length() > 1 && !stopwords.contains(textStr[i].toLowerCase())) {
+//                            writer.write(textStr[i] + "\n");
+//                        }
+//                    }
+//
+//                    System.out.println("Successfully wrote to file >" + "thread" + size + ".txt");
+//                    System.out.println("----------------------------------------");
+//                    writer.close();
+//                } else {
+//                    // avoid duplicate threads
+//                    System.out.println("Skipping " + title + "....");
+//                    System.out.println("----------------------------------------");
+//
+//                }
+//                size++;
+//            }
+//        } catch (ParseException pe) {
+//            System.out.println("position: " + pe.getPosition());
+//            System.out.println(pe);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
